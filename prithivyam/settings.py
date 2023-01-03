6cab3ba2edfa4c48687bb22765d81573405820e3
LyeMuk <mukesh2337.k@gmail.com>
2023-01-03 20:09:53 +0530

    fab icon set, css done

--- prithivyam/settings.py
+++ prithivyam/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jp+nu*v1_y$$%7b3x5lc$$2ug7$j$djyz=dl^ql505n8=t4ny3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
